24f4f21ed6084c1cead52f71f8411859de02cae6
Hzz <xiaofei00152@gmail.com>
2021-11-24 13:10:30 +0800

    浏览记录bug

--- src/main/java/com/zr/web/HistoryController.java
+++ src/main/java/com/zr/web/HistoryController.java
@@ -45,32 +45,41 @@ public class HistoryController {
         String account = String.valueOf(request.getSession().getAttribute("login"));
         // 查询已登录的用户的所有浏览记录
         List<History> histories = historyService.selectPage(account);
-        List<Integer> goodsIdList = new ArrayList<>();
+
         if (histories != null && histories.size() > 0) {
             // 集合转流
             // 对日期排序，降序排序
             // 获取商品id
-            // 限制5个
-            // 收集所有的值
-            goodsIdList = histories.stream()
+            //限制5个
+            // 收集筛选的值
+
+            //要展示的id
+            List<Integer> existList = histories.stream()
                     .sorted(Comparator.comparing(History::getHistoryDate).reversed())
                     .map(History::getGoodsId)
                     .limit(5)
                     .collect(Collectors.toList());
             // 为Integer类型的list集合
-            System.out.println("浏览记录排序：" + goodsIdList);
+            System.out.println("浏览记录排序：" + existList);
             //查询属于此集合的数据
-            List<Goods> goods = goodsService.selectlimit5ListGoods(goodsIdList);
-            // 删除不属于此集合的数据
-            List<Integer> delGoodsIdList = new ArrayList<>();
-            // 收集所有的浏览记录商品id
-            delGoodsIdList = histories.stream().map(History::getGoodsId).collect(Collectors.toList());
+            List<Goods> goods = goodsService.selectlimit5ListGoods(existList);
+
+
+            // 集合转流
+            // 对日期排序，降序排序
+            // 获取浏览记录
+            // 跳过前5个
+            // 收集所有的值
+
+            //要删除的id
+            List<Integer> delList = histories.stream()
+                    .sorted(Comparator.comparing(History::getHistoryDate).reversed())
+                    .map(History::getHistoryId)
+                    .skip(5)
+                    .collect(Collectors.toList());
             //筛选出要删除的浏览记录商品id
-            for (Integer del : goodsIdList) {
-                delGoodsIdList.remove(del);
-            }
             //删除
-            historyService.delHistory(delGoodsIdList);
+            historyService.delHistory(delList);
             result.setResultListObject(goods);
         }
         return result;
